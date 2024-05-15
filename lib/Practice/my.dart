be3ef15ad9c661afe3e5c520a2083210ad4d2dd5
Ankit <ankitumredkar77@gmail.com>
2024-05-15 22:02:37 +0530

    Practice

--- lib/Practice/my.dart
+++ lib/Practice/my.dart
@@ -89,122 +89,122 @@ class _MyClockState extends State<MyClock> {
 
   Container buildContainer(double clockSize) {
     return Container(
-                  height: clockSize,
-                  width: clockSize,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        constraints: const BoxConstraints.expand(),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          gradient: const LinearGradient(colors: [
-                            Color(0xFF1A1A1A),
-                            Color(0xFF121212),
-                          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-                          boxShadow: [
-                            const BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 32,
-                              spreadRadius: 1,
-                              offset: Offset(-20, -10),
-                            ),
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.13),
-                              blurRadius: 32,
-                              spreadRadius: 1,
-                              offset: const Offset(30, 20),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        height: clockSize * 0.465,
-                        width: clockSize * 0.465,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF1A1A1A),
-                          shape: BoxShape.circle,
-                          gradient: const LinearGradient(
-                            colors: [
-                              Color(0xFF1A1A1A),
-                              Color(0xFF121212),
-                            ],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomRight,
-                          ),
-                          boxShadow: [
-                            const BoxShadow(
-                              color: Colors.black,
-                              blurRadius: 32,
-                              spreadRadius: 1,
-                              offset: Offset(-20, -10),
-                            ),
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.13),
-                              blurRadius: 32,
-                              spreadRadius: 1,
-                              offset: const Offset(30, 20),
-                            ),
-                          ],
-                        ),
-                      ),
-                      ...List.generate(
-                        60,
-                            (index) => Transform.rotate(
-                          angle: ((index) * 6 * pi) / 180,
-                          child: (index % 5 == 0)
-                              ? VerticalDivider(
-                            indent: clockSize * 0.04,
-                            endIndent: clockSize * 0.93,
-                            width: 20,
-                            thickness: 3,
-                            color: Colors.grey,
-                          )
-                              : VerticalDivider(
-                            indent: clockSize *0.04,
-                            endIndent: clockSize * 0.92,
-                            color: const Color(0xFF1A1A1A),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 15,
-                        width: 15,
-                        decoration:
-                        const BoxDecoration(shape: BoxShape.circle, color: Colors.red),
-                      ),
-                      Transform.rotate(
-                        angle: ((dateTime.hour + (dateTime.minute / 60)) * 30 * pi / 180),
-                        child: VerticalDivider(
-                          color: Colors.red,
-                          indent: clockSize * 0.29,
-                          endIndent: clockSize * 0.496,
-                          thickness: 4,
-                        ),
-                      ),
-                      Transform.rotate(
-                        angle: (dateTime.minute * 6 * pi) / 180,
-                        child: VerticalDivider(
-                          color: Colors.blue,
-                          indent: clockSize * 0.182,
-                          endIndent: clockSize * 0.496,
-                          thickness: 3,
-                        ),
-                      ),
-                      Transform.rotate(
-                        angle: (dateTime.second * 6 * pi) / 180,
-                        child: VerticalDivider(
-                          color: Colors.grey,
-                          indent: clockSize * 0.12,
-                          endIndent: clockSize * 0.496,
-                          thickness: 1.5,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
+      height: clockSize,
+      width: clockSize,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            constraints: const BoxConstraints.expand(),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: const LinearGradient(colors: [
+                Color(0xFF1A1A1A),
+                Color(0xFF121212),
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 32,
+                  spreadRadius: 1,
+                  offset: Offset(-20, -10),
+                ),
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.13),
+                  blurRadius: 32,
+                  spreadRadius: 1,
+                  offset: const Offset(30, 20),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: clockSize * 0.465,
+            width: clockSize * 0.465,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: const Color(0xFF1A1A1A),
+              shape: BoxShape.circle,
+              gradient: const LinearGradient(
+                colors: [
+                  Color(0xFF1A1A1A),
+                  Color(0xFF121212),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomRight,
+              ),
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 32,
+                  spreadRadius: 1,
+                  offset: Offset(-20, -10),
+                ),
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.13),
+                  blurRadius: 32,
+                  spreadRadius: 1,
+                  offset: const Offset(30, 20),
+                ),
+              ],
+            ),
+          ),
+          ...List.generate(
+            60,
+            (index) => Transform.rotate(
+              angle: ((index) * 6 * pi) / 180,
+              child: (index % 5 == 0)
+                  ? VerticalDivider(
+                      indent: clockSize * 0.04,
+                      endIndent: clockSize * 0.93,
+                      width: 20,
+                      thickness: 3,
+                      color: Colors.grey,
+                    )
+                  : VerticalDivider(
+                      indent: clockSize * 0.04,
+                      endIndent: clockSize * 0.92,
+                      color: const Color(0xFF1A1A1A),
+                    ),
+            ),
+          ),
+          Container(
+            height: 15,
+            width: 15,
+            decoration:
+                const BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+          ),
+          Transform.rotate(
+            angle: ((dateTime.hour + (dateTime.minute / 60)) * 30 * pi / 180),
+            child: VerticalDivider(
+              color: Colors.red,
+              indent: clockSize * 0.29,
+              endIndent: clockSize * 0.496,
+              thickness: 4,
+            ),
+          ),
+          Transform.rotate(
+            angle: (dateTime.minute * 6 * pi) / 180,
+            child: VerticalDivider(
+              color: Colors.blue,
+              indent: clockSize * 0.182,
+              endIndent: clockSize * 0.496,
+              thickness: 3,
+            ),
+          ),
+          Transform.rotate(
+            angle: (dateTime.second * 6 * pi) / 180,
+            child: VerticalDivider(
+              color: Colors.grey,
+              indent: clockSize * 0.12,
+              endIndent: clockSize * 0.496,
+              thickness: 1.5,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
